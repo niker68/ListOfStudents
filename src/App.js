6e7f534d510e3b35480272b2e233bddb808ba563
niker68 <61888868+niker68@users.noreply.github.com>
2021-06-10 00:23:58 +0500

    Searching and deleting added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+import BigList from "./Todo/BigList";
+import SmallList from "./Todo/SmallList";
+import axios from 'axios';
+import ReactDOM from "react-dom";
+import BigListItem from "./Todo/BigListItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+    table:{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding : ".5rem 1rem",
+        border: "1px solid #ccc",
+        borderRadius: "4px",
+        marginBottom: ".5rem"} ,
+    li:{
+        style: "none",
+        display: "flex",
+        alignItems: "center",
+        padding : ".5rem 1rem",
+        border: "1px",
+        borderRadius: "4px",
+        marginBottom: ".5rem"
+    }
 }
 
+function App() {
+    let [studentsArray=[], setStudentsArray] = useState([]);
+    const apiURL = "https://front-assignment-api.2tapp.cc/api/persons";
+    const [textForSearch,setTextForSearch] = useState("");
+    const filteredStudentsBySearch = studentsArray.filter(student =>{
+        return student.name.toLowerCase().includes(textForSearch.toLowerCase())
+    })
+    function deleteStudent (studentsArrayForDelete,name)  {
+        // eslint-disable-next-line array-callback-return
+        const resultArray=[];
+        studentsArrayForDelete.map( (student,id) =>{
+            if(!(student.name === name)){
+                resultArray.push(student)
+            }
+            console.log("push " + student.name)
+        })
+        console.log(resultArray);
+        setStudentsArray(resultArray);
+    }
+
+
+
+    useEffect( async  () => {
+        await fetch(apiURL)
+            .then(response => response.json())
+            .then(data => setStudentsArray(data.students)).catch(error => alert(error.message));
+    }, []);
+    console.log(studentsArray);
+
+    return (
+        <div class="container">
+            <h1 align="center">Students
+            </h1>
+
+                    <div className="dList">
+                        <div className="left">
+                            <form>
+                                <div className="d1">
+                                    <input type="text" id = "text_id" placeholder="Поиск по имени" onChange={(event)=> setTextForSearch(event.target.value)}></input>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="right">
+                            <button className="buttonfilter">фильтры</button>
+                        </div>
+                    <ul>
+                        <li style={styles.li}>
+                            <img className="avatar" ></img>
+                            <span className="fio">ФИО</span>
+                            <span className="job">Специальность</span>
+                            <span className="group">Группа</span>
+                            <span className="age">Возраст</span>
+                            <span className="rating">Рейтинг</span>
+                            <span className="color"></span>
+                            <span>
+                                <button className="buttondelete"  ></button>
+                                </span>
+                        </li>
+                        {filteredStudentsBySearch.map( (student,id) =>{
+
+                            return <li style={styles.li} key = {student.name}>
+                                    <img src={student.avatar} className="avatar" ></img>
+                                <span className="fio">{student.name}
+                                 </span>
+                                <span className="job">{student.specialty}
+                                </span>
+                                <span className="group">{student.group}
+                                </span>
+                                <span className="age">{getAge(student.birthday,new Date()).years}
+                                </span>
+                                <span className="rating">{student.rating}
+                                </span>
+                                <span className="color">{student.color}
+                                </span>
+                                <span>
+                                <button className="buttondelete" onClick={() =>{deleteStudent(studentsArray, student.name)}} >&times;</button>
+                                </span>
+                            </li>
+                        })}
+                    </ul>
+                    </div>
+        </div>
+
+    )
+
+}
+
+function getAge(birthDate, ageAtDate) {
+    var daysInMonth = 30.436875; // Days in a month on average.
+    var dob = new Date(birthDate);
+    var aad;
+    if (!ageAtDate) aad = new Date();
+    else aad = new Date(ageAtDate);
+    var yearAad = aad.getFullYear();
+    var yearDob = dob.getFullYear();
+    var years = yearAad - yearDob; // Get age in years.
+    dob.setFullYear(yearAad); // Set birthday for this year.
+    var aadMillis = aad.getTime();
+    var dobMillis = dob.getTime();
+    if (aadMillis < dobMillis) {
+        --years;
+        dob.setFullYear(yearAad - 1); // Set to previous year birthday
+        dobMillis = dob.getTime();
+    }
+    var days = (aadMillis - dobMillis) / 86400000;
+    var monthsDec = days / daysInMonth; // Months with remainder.
+    var months = Math.floor(monthsDec); // Remove fraction from month.
+    days = Math.floor(daysInMonth * (monthsDec - months));
+    return {years: years, months: months, days: days};
+}
 export default App;
